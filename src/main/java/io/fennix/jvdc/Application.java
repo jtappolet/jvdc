8c86e1e57b32bd7aeff29876c048e7633acc4f30
jtappolet <jtappolet@gmail.com>
2019-08-26 11:37:47 +0000

    adds dependencies

--- src/main/java/io/fennix/jvdc/Application.java
+++ src/main/java/io/fennix/jvdc/Application.java
@@ -1,10 +1,17 @@
 package io.fennix.jvdc;
 
+import com.blade.Blade;
+
 public class Application {
 
     public static void main(String[] args) {
 
-        System.out.println("Test");
+        Blade.of()
+  .get("/basic-routes-example", ctx -> ctx.text("GET called"))
+  .post("/basic-routes-example", ctx -> ctx.text("POST called"))
+  .put("/basic-routes-example", ctx -> ctx.text("PUT called"))
+  .delete("/basic-routes-example", ctx -> ctx.text("DELETE called"))
+  .start(Application.class, args);
 
     }
 
